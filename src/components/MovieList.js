7901372b947e40ecf71c01cf398317eee611dd75
felipeverones <felipe_verones@hotmail.com>
2022-04-15 15:17:09 -0300

    fixes for deploy

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -8,9 +8,8 @@ const MovieList = (props) => {
       {props.movies.map((movie, index) => (
         <div
           key={index}
-          className="image-container d-flex justify-content-start m-3"
-        >
-          <img src={movie.Poster} alt="Movie" />
+          className="image-container d-flex justify-content-start m-3">
+          <img src={movie.Poster} alt="Movie"></img>
           <div
             onClick={() => props.handleFavouritesClick(movie)}
             className="overlay d-flex align-items-center justify-content-center"
